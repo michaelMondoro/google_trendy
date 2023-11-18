7153239d1f6eb622bcabf2fb39822ead5f99f7fe
Michael Mondoro <michaelmondoro@Michaels-MacBook-Air.local>
2023-11-18 11:56:25 -0500

    version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="google_trendy",
-    version="1.0",
+    version="1.1",
     author="Michael Mondoro",
     author_email="michaelmondoro@gmail.com",
     description="Package for getting and analyzing tending Google searches",
