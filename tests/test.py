bd71d32f0a967a4822f8700c9917f12033f5fce9
Michael Mondoro <38412172+michaelMondoro@users.noreply.github.com>
2024-04-09 15:05:22 -0400

    unit tests are awesome :

--- tests/test.py
+++ tests/test.py
@@ -9,7 +9,22 @@ class GoogleTrendsTest(unittest.TestCase):
     def test_init(self):
         trends = GoogleTrends()
         self.assertIsNotNone(trends)
+
+    def test_get_trends(self):
+        trends = GoogleTrends()
+        trends.get_trends(5)
+        self.assertEqual(5, len(trends.trends))
     
+    def test_daily_trends(self):
+        trends = GoogleTrends()
+        self.assertIsNotNone(trends.daily_trends())
+
+    def test_trend_urls(self):
+        trends = GoogleTrends()
+        trends.get_trends(5)
+        for id in trends.trend_ids:
+            self.assertIn("http", trends.trend_url(id))
+
 if __name__ == '__main__':
     unittest.main()
 
